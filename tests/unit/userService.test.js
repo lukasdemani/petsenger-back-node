155f5cd6f01fe6b4065c319b36faa262b0a0e810
demani.lukas@gmail.com <demani.lukas@gmail.com>
2022-06-27 11:08:56 +0100

    feat: test unit

--- tests/unit/userService.test.js
+++ tests/unit/userService.test.js
@@ -1,120 +1,69 @@
-import { faker } from "@faker-js/faker";
-import { jest } from "@jest/globals";
-import bcrypt from "bcrypt";
-import cardRepository, {
-  CardInsertData,
-  TransactionTypes,
-} from "../../src/repositories/cardRepository.js";
-import companyRepository from "../../src/repositories/companyRepository.js";
-import employeeRepository, {
-  Employee,
-} from "../../src/repositories/employeeRepository.js";
-import cardService from "../../src/services/cardService.js";
+import { prisma } from '@/config';
+import userService, { duplicatedEmailError } from '@/services/users-service';
+import faker from '@faker-js/faker';
+import bcrypt from 'bcrypt';
+import { prisma } from "../../src/database.js";
+import userBodyFactory from "../factories/userBodyFactory.js";
+import { signUp, signIn } from "../../src/services/userService.js"
 
-describe("Card service unit tests", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    jest.resetAllMocks();
+
+describe('signUp', () => {
+  beforeEach(truncateUsers);
+
+  afterAll(disconnect);
+
+  it('should throw duplicatedUserError if there is a user with given email', async () => {
+    const existingUser = userBodyFactory;
+
+    try {
+      await signUp({
+        firstName: existingUser.firstName,
+        lastName: existingUser.lastName,
+        email: existingUser.email,
+        password: faker.internet.password(6),
+      });
+      fail('should throw duplicatedUserError');
+    } catch (error) {
+      expect(error).toEqual(duplicatedEmailError());
+    }
   });
 
-  it("should create a card", async () => {
-    const apiKey = "12345";
-    const employeeId = 1;
-    const cardType: TransactionTypes = "groceries";
-    const cardCVC = "123";
-    const cardNumber = "6771-8904-7736-0214";
-    const employee: Employee = {
-      id: employeeId,
-      fullName: "mateus moura",
-      email: "dina@email.com",
-      cpf: "47100946832",
-      companyId: 1,
-    };
+  it('should create user when given email is unique', async () => {
+    user = userBodyFactory;
+    const user = await signUp(user);
 
-    jest.spyOn(faker.finance, "creditCardNumber").mockReturnValue(cardNumber);
-    jest.spyOn(faker.finance, "creditCardCVV").mockReturnValue(cardCVC);
-    jest.spyOn(bcrypt, "hashSync").mockReturnValue(cardCVC);
-
-    const cardData = cardService.generateCardData(employee.fullName);
-    const expectedCard: CardInsertData = {
-      ...cardData,
-      isBlocked: false,
-      isVirtual: false,
-      employeeId,
-      type: cardType,
-    };
-
-    jest
-      .spyOn(companyRepository, "findByApiKey")
-      .mockResolvedValue(companyFactory(apiKey));
-    jest.spyOn(employeeRepository, "findById").mockResolvedValue(employee);
-    jest
-      .spyOn(cardRepository, "findByTypeAndEmployeeId")
-      .mockResolvedValue(null);
-    const cardRepositoryInsert = jest
-      .spyOn(cardRepository, "insert")
-      .mockResolvedValue(null);
-
-    await cardService.create(apiKey, employeeId, cardType);
-
-    expect(cardRepositoryInsert).toBeCalledTimes(1);
-    expect(cardRepositoryInsert).toBeCalledWith(expectedCard);
+    const dbUser = await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+    expect(user).toEqual(
+      expect.objectContaining({
+        id: dbUser.id,
+        email: dbUser.email,
+      }),
+    );
   });
 
-  it("should create a card with the correct abbreviated name", async () => {
-    const apiKey = "12345";
-    const employeeId = 1;
-    const cardType: TransactionTypes = "groceries";
-    const cardCVC = "123";
-    const cardNumber = "6771-8904-7736-0214";
-    const employee: Employee = {
-      id: employeeId,
-      fullName: "mateus de nardo moura",
-      email: "dina@email.com",
-      cpf: "47100946832",
-      companyId: 1,
-    };
+  it('should hash user password', async () => {
+    const rawPassword = faker.internet.password(6);
+    user = userBodyFactory;
+    const user = await userService.createUser(user);
 
-    jest.spyOn(faker.finance, "creditCardNumber").mockReturnValue(cardNumber);
-    jest.spyOn(faker.finance, "creditCardCVV").mockReturnValue(cardCVC);
-    jest.spyOn(bcrypt, "hashSync").mockReturnValue(cardCVC);
-
-    const cardData = cardService.generateCardData(employee.fullName);
-    const expectedCard: CardInsertData = {
-      ...cardData,
-      isBlocked: false,
-      isVirtual: false,
-      employeeId,
-      type: cardType,
-    };
-
-    jest
-      .spyOn(companyRepository, "findByApiKey")
-      .mockResolvedValue(companyFactory(apiKey));
-    jest.spyOn(employeeRepository, "findById").mockResolvedValue(employee);
-    jest
-      .spyOn(cardRepository, "findByTypeAndEmployeeId")
-      .mockResolvedValue(null);
-    const cardRepositoryInsert = jest
-      .spyOn(cardRepository, "insert")
-      .mockResolvedValue(null);
-
-    await cardService.create(apiKey, employeeId, cardType);
-
-    expect(cardRepositoryInsert).toBeCalledTimes(1);
-    expect(cardRepositoryInsert).toBeCalledWith(expectedCard);
+    const dbUser = await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+    expect(dbUser.password).not.toBe(rawPassword);
+    expect(await bcrypt.compare(rawPassword, dbUser.password)).toBe(true);
   });
-
-  it.todo("should not create a card given invalid api key");
-
-  it.todo("should activate a card");
-  it.todo("should return a card");
 });
 
-function companyFactory(apiKey: string) {
-  return {
-    id: 1,
-    name: "driven",
-    apiKey,
-  };
+async function disconnect() {
+  await prisma.$disconnect();
+}
+
+async function truncateUsers() {
+  await prisma.$executeRaw`TRUNCATE TABLE users CASCADE;`;
 }
